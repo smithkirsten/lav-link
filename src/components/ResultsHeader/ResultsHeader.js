29ad6c1ad51f43c05833f33fa64f0d01a1e93db1
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-02-18 14:38:32 -0500

    Add classNames to ResultsHeader

--- src/components/ResultsHeader/ResultsHeader.js
+++ src/components/ResultsHeader/ResultsHeader.js
@@ -1,19 +1,26 @@
 import React from "react";
 import './ResultsHeader.css'
-
+//want forms to hold state for onChange (user typing) 
+//should display checkboxes as user selected them on homepage
 const ResultsHeader = () => {
+  const zip = 'zipcode'
   return (
-    <header>
-      <h1></h1>
-      <p></p>
-      <p></p>
-      <form>
-        <input></input>
-        <input></input>
-        <input></input>
-        <input></input>
-        <input></input>
-        <button></button>
+    <header className='results-header'>
+      <h1 className='heading'>LavLink</h1>
+      <p className='showing'>showing results for...</p>
+      <p className='zip'>{zip}</p>
+      <form className='filterForm'>
+        <label for='zipcode' className='hidden input-label text-label'>zipcode</label>
+        <input type='text' name='zipcode' placeholder='zipcode...'></input>
+        <label for='ada' className='input-label'>image here</label>
+        <input type='checkbox' name='ada'></input>
+        <label for='unisex' className='input-label'>image here</label>
+        <input type='checkbox' name='unisex'></input>
+        <label for='unisex' className='input-label'>image here</label>
+        <input type='checkbox' name='family'></input>
+        <label for='numResults' className='input-label text-label'>max results</label>
+        <input type='number' name='numResults' min='5' max='100'></input>
+        <button className='changeButton'>change</button>
       </form>
     </header>
   )
