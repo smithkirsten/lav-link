693e9578e1d887a29923e90dd8b9c794166487e3
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-02-18 13:28:05 -0500

    Move header to ResultsHeader component

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -6,19 +6,7 @@ import './Results.css'
 const Results = () => {
   return (
     <>
-      <header>
-        <h1></h1>
-        <p></p>
-        <p></p>
-        <form>
-          <input></input>
-          <input></input>
-          <input></input>
-          <input></input>
-          <input></input>
-          <button></button>
-        </form>
-      </header>
+
       <section>
       </section>
     </>
--- src/components/ResultsHeader/ResultsHeader.js
+++ src/components/ResultsHeader/ResultsHeader.js
@@ -2,7 +2,21 @@ import React from "react";
 import './ResultsHeader.css'
 
 const ResultsHeader = () => {
-
+  return (
+    <header>
+      <h1></h1>
+      <p></p>
+      <p></p>
+      <form>
+        <input></input>
+        <input></input>
+        <input></input>
+        <input></input>
+        <input></input>
+        <button></button>
+      </form>
+    </header>
+  )
 }
 
 export default ResultsHeader
