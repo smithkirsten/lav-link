743ac3c7276ed242c3d9299934f19fe3d5129a07
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-02-18 13:45:00 -0500

    Add basic JSX structure for ResultCard

--- src/components/ResultCard/ResultCard.js
+++ src/components/ResultCard/ResultCard.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import './ResultCard.css'
 
-const ResultCard = () => {
+const dummyBathroom = { name: 'Establishment Name', distance: '0.5', upvote: '2'}
 
+
+const ResultCard = () => {
+  return (
+    <article className='result-card'>
+      <img />
+      <p className='result-name'>{dummyBathroom.name}</p>
+      <p className='result-distance'>{dummyBathroom.distance}</p>
+      <p className='result-upvote'>{dummyBathroom.upvote}</p>
+    </article>
+  )
 }
 
 export default ResultCard
