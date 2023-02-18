43df5e5969f3b4d373358259ae09430e9ca0e63a
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-02-18 13:51:22 -0500

    Sort results by distance

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -5,13 +5,15 @@ import './Results.css'
 
 const dummyBathroom = { name: 'Establishment Name', distance: '0.5', upvote: '2'}
 const dummyResults = [ dummyBathroom, dummyBathroom, dummyBathroom, dummyBathroom, dummyBathroom ]
+//order results by distance or by upvote? but Rae said a lot of them dont have upvotes
 
+//did not insert props because....store vs props?
 const Results = () => {
-  const cards = dummyResults.map(result => <Card />)
+  const cards = dummyResults.map(result => <ResultCard />).sort((a, b) => a.distance - b.distance)
   return (
     <>
       <ResultsHeader />
-      <section>
+      <section className='cards-display'>
         {cards}
       </section>
     </>
