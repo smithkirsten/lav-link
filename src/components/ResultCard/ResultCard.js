3c88d5403427222958f3d55dceb298d859b0c88f
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-02-18 13:17:05 -0500

    Add imports and export for ResultCard

---
+++ src/components/ResultCard/ResultCard.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './ResultCard.css'
+
+const ResultCard = () => {
+
+}
+
+export default ResultCard
--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ResultCard from "../ResultCard/ResultCard";
 import './Results.css'
 
 const Results = () => {
