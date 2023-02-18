9303b9041bf5c0f95dd09ad465d1fb9587c0895c
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-02-18 16:09:59 -0500

    Remove default margins and padding

--- src/components/ResultCard/ResultCard.js
+++ src/components/ResultCard/ResultCard.js
@@ -7,7 +7,7 @@ const dummyBathroom = { name: 'Establishment Name', distance: '0.5', upvote: '2'
 const ResultCard = () => {
   return (
     <article className='result-card'>
-      <img />
+      <img src='/assets/toilet.png' alt='toilet icon'/>
       <p className='result-name'>{dummyBathroom.name}</p>
       <p className='result-distance'>{dummyBathroom.distance}</p>
       <p className='result-upvote'>{dummyBathroom.upvote}</p>
