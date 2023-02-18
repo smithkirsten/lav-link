1650849a3e44ef4c8d84bc7a61ba3042ef421f49
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-02-18 13:34:45 -0500

    Add dummy cards to Results

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -3,11 +3,16 @@ import ResultsHeader from '../ResultsHeader/ResultsHeader'
 import ResultCard from "../ResultCard/ResultCard";
 import './Results.css'
 
+const dummyBathroom = { name: 'Establishment Name', distance: '0.5', upvote: '2'}
+const dummyResults = [ dummyBathroom, dummyBathroom, dummyBathroom, dummyBathroom, dummyBathroom ]
+
 const Results = () => {
+  const cards = dummyResults.map(result => <Card />)
   return (
     <>
-
+      <ResultsHeader />
       <section>
+        {cards}
       </section>
     </>
   )
