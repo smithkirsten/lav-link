011fbee5e3730e2dad63defc8c642b485ce358d6
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-02-18 17:38:02 -0500

    Add state to form

--- src/components/ResultsHeader/ResultsHeader.js
+++ src/components/ResultsHeader/ResultsHeader.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './ResultsHeader.css'
 //want forms to hold state for onChange (user typing) but send that state to the store onClick 
-//should display checkboxes as user selected them on homepage
+//should display checkboxes as user selected them on homepage(will get that info from store and display accordingly on update)
+
+
+
 const ResultsHeader = () => {
-  const zip = 'zipcode'
+  const [ zipcode, setZipcode ] = useState('')
+  const [ ada, setAda ] = useState(false) //will be grabbed from store
+  const [ unisex, setUnisex ] = useState(false) //will be grabbed from store
+  const [ changingTable, setChangingTable ] = useState(false) //will be grabbed from store
+  const zip = 'zipcode' //this will come from the store
   return (
     <header className='results-header'>
       <h1 className='heading'>LavLink</h1>
@@ -11,16 +18,14 @@ const ResultsHeader = () => {
       <p className='zip'>{zip}</p>
       <form className='filterForm'>
         <div>
-          <label for='zipcode' className='hidden input-label text-label'>zipcode</label>
-          <input type='text' name='zipcode' placeholder='zipcode...' className='zip-input'></input>
-          <label for='ada' className='input-label'><img src='/assets/wheelchair.png'/></label>
-          <input type='checkbox' name='ada'></input>
-          <label for='unisex' className='input-label'><img src='/assets/transgender.png'/></label>
-          <input type='checkbox' name='unisex'></input>
-          <label for='changingTable' className='input-label'><img src='/assets/baby.png'/></label>
-          <input type='checkbox' name='changingTable'></input>
-          <label for='numResults' className='input-label text-label'>show</label>
-          <input type='number' name='numResults' min='5' max='100' className='num-input' placeholder='5'></input>
+          <label htmlFor='zipcode' className='hidden input-label text-label'>zipcode</label>
+            <input type='text' name='zipcode' value={zipcode} onChange={(event) => setZipcode(event.target.value)}placeholder='zipcode...' className='zip-input'></input>
+          <label htmlFor='ada' className='input-label'><img src='/assets/wheelchair.png'/></label>
+            <input type='checkbox' name='ada' checked={ada} onChange={(event) => setAda(event.target.value)}></input>
+          <label htmlFor='unisex' className='input-label'><img src='/assets/transgender.png'/></label>
+            <input type='checkbox' name='unisex' checked={unisex} onChange={(event) => setUnisex(event.target.value)}></input>
+          <label htmlFor='changingTable' className='input-label'><img src='/assets/baby.png'/></label>
+            <input type='checkbox' name='changingTable' checked={changingTable} onChange={(event) => setChangingTable(event.target.value)}></input>
         </div>
         <button className='changeButton'>change</button>
       </form>
@@ -28,5 +33,7 @@ const ResultsHeader = () => {
     </header>
   )
 }
+//currently checkboxes can be checked but not unchecked
+  //state reads as "on"
 
 export default ResultsHeader
