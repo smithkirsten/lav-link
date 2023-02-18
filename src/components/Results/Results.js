f8c8280d27a2ed9c43e3f1a2b5d6a8fdb27431f1
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-02-18 13:22:12 -0500

    Add ResultsHeader component and imports

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ResultsHeader from '../ResultsHeader/ResultsHeader'
 import ResultCard from "../ResultCard/ResultCard";
 import './Results.css'
 
@@ -6,6 +7,17 @@ const Results = () => {
   return (
     <>
       <header>
+        <h1></h1>
+        <p></p>
+        <p></p>
+        <form>
+          <input></input>
+          <input></input>
+          <input></input>
+          <input></input>
+          <input></input>
+          <button></button>
+        </form>
       </header>
       <section>
       </section>
---
+++ src/components/ResultsHeader/ResultsHeader.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './ResultsHeader.css'
+
+const ResultsHeader = () => {
+
+}
+
+export default ResultsHeader
