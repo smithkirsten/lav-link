91d4eb0dace27a23c087d4456761453bc1b1d921
Rae Gebhart <raegebhart@gmail.com>
2023-02-18 11:12:49 -0700

    Import images

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,32 +1,36 @@
-import React from 'react'
-
-import './Details.css'
+import React from "react";
+import wheelchair from "../../assets/wheelchair.png";
+import transgender from "../../assets/transgender.png";
+import baby from "../../assets/baby.png";
+import "./Details.css";
 
 const Details = () => {
   return (
     <section>
-      <section className='top'>
-        <section className='details'>
+      <section className="top">
+        <section className="details">
           <p>Name</p>
           <p>Distance</p>
-          <div className='icon-container'>
-
+          <div className="icon-container">
+            <img className="icon wheelchair" src={wheelchair}></img>
+            <img className="icon transgender" src={transgender}></img>
+            <img className="icon baby" src={baby}></img>
           </div>
         </section>
-        <section className='details-map'>This is a map... trust me</section>
+        <section className="details-map">This is a map... trust me</section>
       </section>
-      <summary className='summary'>
+      <summary className="summary">
         <p>address</p>
         <p>description</p>
       </summary>
-      <section className='conclusion'>
+      <section className="conclusion">
         <p>comment</p>
         <p>Upvotes</p>
         <p>Downvotes</p>
         <p>Last Updated</p>
       </section>
     </section>
-  )
-}
+  );
+};
 
-export default Details
+export default Details;
