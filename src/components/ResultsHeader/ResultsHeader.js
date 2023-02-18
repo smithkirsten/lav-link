f618ad318a294cdae1075554872d8ff08f448e3d
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-02-18 17:02:28 -0500

    Style form further

--- src/components/ResultsHeader/ResultsHeader.js
+++ src/components/ResultsHeader/ResultsHeader.js
@@ -10,18 +10,21 @@ const ResultsHeader = () => {
       <p className='showing'>showing results for...</p>
       <p className='zip'>{zip}</p>
       <form className='filterForm'>
-        <label for='zipcode' className='hidden input-label text-label'>zipcode</label>
-        <input type='text' name='zipcode' placeholder='zipcode...' className='zip-input'></input>
-        <label for='ada' className='input-label'><img src='/assets/wheelchair.png'/></label>
-        <input type='checkbox' name='ada'></input>
-        <label for='unisex' className='input-label'><img src='/assets/transgender.png'/></label>
-        <input type='checkbox' name='unisex'></input>
-        <label for='changingTable' className='input-label'><img src='/assets/baby.png'/></label>
-        <input type='checkbox' name='changingTable'></input>
-        <label for='numResults' className='input-label text-label'>show</label>
-        <input type='number' name='numResults' min='5' max='100' className='num-input' placeholder='5'></input>
+        <div>
+          <label for='zipcode' className='hidden input-label text-label'>zipcode</label>
+          <input type='text' name='zipcode' placeholder='zipcode...' className='zip-input'></input>
+          <label for='ada' className='input-label'><img src='/assets/wheelchair.png'/></label>
+          <input type='checkbox' name='ada'></input>
+          <label for='unisex' className='input-label'><img src='/assets/transgender.png'/></label>
+          <input type='checkbox' name='unisex'></input>
+          <label for='changingTable' className='input-label'><img src='/assets/baby.png'/></label>
+          <input type='checkbox' name='changingTable'></input>
+          <label for='numResults' className='input-label text-label'>show</label>
+          <input type='number' name='numResults' min='5' max='100' className='num-input' placeholder='5'></input>
+        </div>
         <button className='changeButton'>change</button>
       </form>
+      
     </header>
   )
 }
