e3ccbc9b3529b8705c2937b5f9bf02e8aa3c7608
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-02-18 16:20:59 -0500

    Style ResultCard

--- src/components/ResultCard/ResultCard.js
+++ src/components/ResultCard/ResultCard.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './ResultCard.css'
 
-const dummyBathroom = { name: 'Establishment Name', distance: '0.5', upvote: '2'}
+const dummyBathroom = { name: 'Establishment', distance: '0.5', upvote: '2'}
 
 
 const ResultCard = () => {
@@ -9,8 +9,8 @@ const ResultCard = () => {
     <article className='result-card'>
       <img src='/assets/toilet.png' alt='toilet icon'/>
       <p className='result-name'>{dummyBathroom.name}</p>
-      <p className='result-distance'>{dummyBathroom.distance}</p>
-      <p className='result-upvote'>{dummyBathroom.upvote}</p>
+      <p className='result-distance'>{dummyBathroom.distance} miles away</p>
+      <p className='result-upvote'>{dummyBathroom.upvote} ⬆️ </p>
     </article>
   )
 }
