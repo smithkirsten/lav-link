6dba9caf97d863f0489bba3915d518ef7a7fff47
Rick Vermeil <rickv85@gmail.com>
2023-02-21 17:46:32 -0700

    Add intercept and fixture for get request and test unfiltered results

--- cypress/e2e/landing.cy.js
+++ cypress/e2e/landing.cy.js
@@ -1,5 +1,9 @@
 describe('template spec', () => {
   beforeEach(() => {
+    cy.intercept('GET', 'https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=30&offset=0&lat=40.051112&lng=-105.027632',
+    {
+      fixture: '../fixtures/get80516request'
+    } )
     cy.visit('http://localhost:3000/')
   })
 
@@ -30,4 +34,28 @@ describe('template spec', () => {
     cy.get('label[for="changingTable"]').should('be.visible')
     cy.get('button[name="searchButton"]').should('be.visible')
   })
+
+  it('Should direct you to the search results page submitting a valid zipcode', () => {
+    cy.get('input[name="zipcodeInput"]').type('80516')
+    cy.get('button[name="searchButton"]').click()
+    cy.url().should('eq', 'http://localhost:3000/results')
+  })
+
+  it('Should display appropriate results on results page after submitting zipcode 80516', () => {
+    cy.get('input[name="zipcodeInput"]').type('80516')
+    cy.get('button[name="searchButton"]').click()
+    cy.get('article[class="result-card"]').eq(0).should('contain', 'Erie Community Library')
+    cy.get('article[class="result-card"]').eq(1).should('contain', 'Erie Public Library')
+    cy.get('article[class="result-card"]').eq(2).should('contain', 'Mod Pizza')
+  })
+
+  // We will need a similar test to above with current location selection once we have it
+  // That will be interesting with the stub because the request will vary depending on where the person is that runs it
+  // Assert that first three contain text that they each should
+
+  // Once we have implemented DOM filtering, add each filter in individual tests and assert the first
+  // Assert that first three contain text that they each should
+
+  // Test combinations of filters like above
+
 })
