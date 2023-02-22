32e7fdae78ccc54193d9f57bd17cf6d04eb01dd1
Tristin Sorrells <Tristinsorrells1@gmail.com>
2023-02-22 13:58:32 -0600

    Remove dynamic path for result cards

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/results" element={<Results />} />
-        <Route path="/results/bathroom" element={<Details />} />
+        <Route path="/results/details" element={<Details />} />
         <Route path="*" element={<Error />} />
       </Routes>
     </main>
