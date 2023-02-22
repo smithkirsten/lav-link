360f9cdb2d73b9837281fd349d2c080f0679652b
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-02-22 14:56:22 -0500

    I dont know I did a lot of things and they didnt work

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ResultsHeader from '../ResultsHeader/ResultsHeader'
 import ResultCard from "../ResultCard/ResultCard";
 import './Results.css'
@@ -12,9 +12,6 @@ const Results = () => {
   const adaAccessible = useSelector((state) => state.landing.adaAccessible)
   const unisex = useSelector((state) => state.landing.unisex)
   const changingTable = useSelector((state) => state.landing.changingTable)
-
-  //add a local state that listens for changes to any of the below variables
-    //state will hold filtered results
   //fetch request data
   const {
     data: results,
@@ -26,6 +23,8 @@ const Results = () => {
 
 const filter = (results) => {
   //make more dynamic... pass in an array of what to filter instead of referencing the global variables?
+    //if filters are passed it, can move filter function to util file
+    console.log('about to filter: ', results)
 
   if(adaAccessible){
     console.log('filtering for ada')
@@ -38,38 +37,71 @@ const filter = (results) => {
     console.log(results)
   }
   if(changingTable) {
+    
     console.log('filtering for big baby')
     results = results.filter(result => result.changing_table)
     console.log(results)
   }
-  console.log(filtered)
-  return filtered;
+  console.log('returning: ', results)
+  return results;
 }
   //local state
-const [filteredResults, setFilteredResults] = useState([]);
-  
-useEffect(() => {
-  setFilteredResults(filter(results))
-}, [ gpsCoordinates, adaAccessible, unisex, changingTable ])
+  const [filteredResults, setFilteredResults] = useState([]);
 
-//display logic
+// useEffect(() => {
+//   //trigger a re-get if gpsCoordinates change
+//   setFilteredResults(filter(results))
+// }, [ gpsCoordinates, adaAccessible, unisex, changingTable ])
+
+// useEffect(() => {
+//   content = filteredResults.map((result) => <ResultCard key={result.id} data={result} />)
+// }, [filteredResults])
+
+// useEffect(() => {
+//   setFilteredResults(filter(results))
+// }, [results])
+
+
+
+//add results to global state
+
 let content
+// content = filteredResults.map((result) => <ResultCard key={result.id} data={result} />)
+
+// if (isSuccess) {
+//   setFilteredResults(filter(results))
+//   const boop = filteredResults
+//   content = boop.map((result) => <ResultCard key={result.id} data={result} />)
+//   //put links around individual cards
+//   // content = filteredResults.map((result) => <ResultCard key={result.id} data={result} />);
+// } 
+
+// display logic
 if (isLoading) {
   //loading page is later problem
   content = <h2>Loading ...</h2>;
 } else if (isSuccess) {
-  console.log(results)
-  //filter results
-    //grab filters from state and determine which to apply
-      //create filter functions in util file
+  setFilteredResults(filter(results))
+  const copy = filteredResults
+  content = copy.map((result) => <ResultCard key={result.id} data={result} />)
   //put links around individual cards
-
-  content = filteredResults.map((result) => <ResultCard key={result.id} data={result} />);
+  // content = filteredResults.map((result) => <ResultCard key={result.id} data={result} />);
 } else if (isError) {
   //Rae is making error page
   content = <h2>Error city</h2>
 }
   return (
+    // <>
+    //   <ResultsHeader />
+    //   <section className='cards-display'>
+    //     <div className="cards-container">
+    //       {isError && <p>error city</p>}
+    //       {isLoading && <p>loading....</p>}
+    //       {filteredResults && content}
+    //     </div>
+    //   </section>
+    // </>
+
     <>
       <ResultsHeader />
       <section className='cards-display'>
