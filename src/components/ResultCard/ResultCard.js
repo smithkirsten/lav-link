86305fe0d735449672eef5380dc0cc66af14106e
Tristin Sorrells <Tristinsorrells1@gmail.com>
2023-02-21 19:35:54 -0600

    Add onClick function to dispatch selectBathroom when a user clicks on a result card

--- src/components/ResultCard/ResultCard.js
+++ src/components/ResultCard/ResultCard.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import './ResultCard.css'
 import { NavLink } from "react-router-dom";
+import { useDispatch } from 'react-redux'
+import { selectBathroom } from './resultSlice.js'
 
 const ResultCard = ({data}) => {
+  const dispatch = useDispatch();
   return (
-    <NavLink to={`/results/${data.id}`}>
+    <NavLink to={`/results/${data.id}`} onClick={() => dispatch(selectBathroom(data))}>
       <article className="result-card">
         <img src="/assets/toilet.png" alt="toilet icon" />
         <p className="result-name">{data.name}</p>
