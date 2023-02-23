1cf7caa93cdb0e22f5c841204e5224f53515943f
Tristin Sorrells <Tristinsorrells1@gmail.com>
2023-02-23 11:39:26 -0600

    Fix merge conflicts

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,22 +1,20 @@
 import React, { useEffect, useState } from "react";
-import ResultsHeader from '../ResultsHeader/ResultsHeader'
+import ResultsHeader from "../ResultsHeader/ResultsHeader";
 import ResultCard from "../ResultCard/ResultCard";
-import './Results.css'
-import { useDispatch, useSelector } from 'react-redux'
+import "./Results.css";
+import { useDispatch, useSelector } from "react-redux";
 import { useGetLavsQuery } from "../../apicalls";
 import { updateSearchResults } from "./searchSlice";
-
-
 
 const Results = () => {
   //global state data
   //add currentCoordinates
   const dispatch = useDispatch();
-  const gpsCoordinates = useSelector((state) => state.landing.gpsCoordinates)
-  const adaAccessible = useSelector((state) => state.landing.adaAccessible)
-  const unisex = useSelector((state) => state.landing.unisex)
-  const changingTable = useSelector((state) => state.landing.changingTable)
-  const searchResults = useSelector((state) => state.search.searchResults)
+  const gpsCoordinates = useSelector((state) => state.landing.gpsCoordinates);
+  const adaAccessible = useSelector((state) => state.landing.adaAccessible);
+  const unisex = useSelector((state) => state.landing.unisex);
+  const changingTable = useSelector((state) => state.landing.changingTable);
+  const searchResults = useSelector((state) => state.search.searchResults);
 
   //const [ allResults, setAllResults ] = useState([]) //this will change to a dispatch to update global instead
   const [filteredResults, setFilteredResults] = useState([]);
@@ -29,53 +27,52 @@ const Results = () => {
     error,
   } = useGetLavsQuery(gpsCoordinates);
 
-let temp
+  let temp;
 
-useEffect(() => {
-  if(isLoading) {
-    temp = <p>loading....</p>
-  } else if(isError) {
-    temp = <p>Error City</p>
-  } else if(isSuccess) {
-    dispatch(updateSearchResults(results))
-    //setAllResults(results)
-  }
-})
+  useEffect(() => {
+    if (isLoading) {
+      temp = <p>loading....</p>;
+    } else if (isError) {
+      temp = <p>Error City</p>;
+    } else if (isSuccess) {
+      dispatch(updateSearchResults(results));
+      //setAllResults(results)
+    }
+  });
 
-useEffect(() => {
-  setFilteredResults(filter(searchResults))
-}, [searchResults])
+  useEffect(() => {
+    setFilteredResults(filter(searchResults));
+  }, [searchResults]);
 
-const createCards = () => {
-  return filteredResults.map((result) => <ResultCard key={result.id} data={result} />)
-}
+  const createCards = () => {
+    return filteredResults.map((result) => (
+      <ResultCard key={result.id} data={result} />
+    ));
+  };
 
-const filter = (results) => {
-  //make more dynamic... pass in an array of what to filter instead of referencing the global variables?
+  const filter = (results) => {
+    //make more dynamic... pass in an array of what to filter instead of referencing the global variables?
     //if filters are passed it, can move filter function to util file
-    console.log('about to filter: ', results)
+    console.log("about to filter: ", results);
 
-  if(adaAccessible){
-    console.log('filtering for ada')
-    results = results.filter(result => result.accessible)
-    console.log(results)
-  }
-  if(unisex) {
-    console.log('filtering for unisex')
-    results = results.filter(result => result.unisex)
-    console.log(results)
-  }
-  if(changingTable) {
-    
-    console.log('filtering for big baby')
-    results = results.filter(result => result.changing_table)
-    console.log(results)
-  }
-  console.log('returning: ', results)
-  return results;
-}
-
-
+    if (adaAccessible) {
+      console.log("filtering for ada");
+      results = results.filter((result) => result.accessible);
+      console.log(results);
+    }
+    if (unisex) {
+      console.log("filtering for unisex");
+      results = results.filter((result) => result.unisex);
+      console.log(results);
+    }
+    if (changingTable) {
+      console.log("filtering for big baby");
+      results = results.filter((result) => result.changing_table);
+      console.log(results);
+    }
+    console.log("returning: ", results);
+    return results;
+  };
 
   return (
     <>
@@ -88,73 +85,9 @@ const filter = (results) => {
         </div>
       </section>
     </>
-
-    // <>
-    //   <ResultsHeader />
-    //   <section className='cards-display'>
-    //     <div className="cards-container">
-    //       {isError && <p>error city</p>}
-    //       {isLoading && <p>loading....</p>}
-    //       {filteredResults && content}
-    //     </div>
-    //   </section>
-    // </>
-
-    // <>
-    //   <ResultsHeader />
-    //   <section className='cards-display'>
-    //     <div className="cards-container">
-    //       {content}
-    //     </div>
-    //   </section>
-    // </>
+    
   );
+};
 
-}
+export default Results;
 
-export default Results
-
-
-
-
-
-
-
-// useEffect(() => {
-//   //trigger a re-get if gpsCoordinates change
-//   setFilteredResults(filter(results))
-// }, [ gpsCoordinates, adaAccessible, unisex, changingTable ])
-
-// useEffect(() => {
-//   content = filteredResults.map((result) => <ResultCard key={result.id} data={result} />)
-// }, [filteredResults])
-
-// useEffect(() => {
-//   setFilteredResults(filter(results))
-// }, [results])
-
-
-// content = filteredResults.map((result) => <ResultCard key={result.id} data={result} />)
-
-// if (isSuccess) {
-//   setFilteredResults(filter(results))
-//   const boop = filteredResults
-//   content = boop.map((result) => <ResultCard key={result.id} data={result} />)
-//   //put links around individual cards
-//   // content = filteredResults.map((result) => <ResultCard key={result.id} data={result} />);
-// } 
-// let temp;
-// // display logic
-// if (isLoading) {
-//   //loading page is later problem
-//   temp = <h2>Loading ...</h2>;
-// } else if (isError) {
-//   //Rae is making error page
-//   temp = <h2>Error city</h2>
-// } else if (isSuccess) {
-    // setFilteredResults(filter(results))
-  // const copy = filteredResults
-  // content = filteredResults.map((result) => <ResultCard key={result.id} data={result} />)
-  //put links around individual cards
-  // content = filteredResults.map((result) => <ResultCard key={result.id} data={result} />);
-//}
