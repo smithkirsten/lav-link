ea39cf6833e6e3387f8f656fdaccdb7ca701c21f
Rae Gebhart <raegebhart@gmail.com>
2023-02-22 17:41:38 -0700

    Create cleaning function

--- src/util.js
+++ src/util.js
@@ -22,3 +22,26 @@ export const reformatDate = (bathroom) => {
 export const validateZip = zip => {
   return /^\d{5}$/.test(zip);
 }
+
+export const cleanData = data => {
+  return data.map(data => {
+    return ({
+      "id": data.id,
+      "name": data.name,
+      "street": data.street,
+      "city": data.city,
+      "state": data.state,
+      "accessible": data.accessible,
+      "unisex": data.unisex,
+      "directions": data.directions,
+      "comment": data.comment,
+      "latitude": data.latitude,
+      "longitude": data.longitude,
+      "update_at": data.update_at,
+      "downvote": data.downvote,
+      "upvote": data.upvote,
+      "country": data.country,
+      "changing_table": data.changing_table,
+    })
+  })
+}
