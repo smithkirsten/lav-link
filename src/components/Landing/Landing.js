6e31cf765b691262024bc9d1781baf2ee997574b
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-02-24 09:32:52 -0500

    Disable zipcode input on landing page if currentLocation is checked

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -73,7 +73,7 @@ export default function Landing() {
       return null;
     }
   }
-
+  let isDisabled = currentLocation ? true : false
   return (
     <section className="landing-main">
       <section className="landing-content">
@@ -104,6 +104,7 @@ export default function Landing() {
           maxLength={5}
           value={zipcode}
           onChange={(event) => setZipcode(event.target.value)}
+          disabled={isDisabled}
         />
         {errorMessageDisplay()}
         <section className="filter-section">
