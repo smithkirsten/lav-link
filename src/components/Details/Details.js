b39519e0e7f71de88304c008372c4b832a494311
Kirsten Smith <brownsmith.kirsten@gmail.com>
2023-02-26 10:30:00 -0500

    Comment out useEffect import to satisfy linter

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+// import React, { useEffect } from "react";
 import "./Details.css";
 import { useSelector } from "react-redux";
 // import { roundDistance, reformatDate } from "../../util";
